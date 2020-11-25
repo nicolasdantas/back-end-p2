fe74e3cb4ae7388f2723baf4b14e9f907712555c
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-25 18:05:31 +0100

    port changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const connection = require('./config');
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
